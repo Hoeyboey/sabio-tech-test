9a7aa6c4231e2bf426423505f3a87ac8adce9397
Hannah Dwan <hannahdwan96@gmail.com>
2021-03-29 17:55:19 +0100

    Fix migration - table name incorrect

--- acme-voip/db/migrate/20210329163231_add_customer_relation_to_device.rb
+++ acme-voip/db/migrate/20210329163231_add_customer_relation_to_device.rb
@@ -1,5 +1,5 @@
 class AddCustomerRelationToDevice < ActiveRecord::Migration[6.0]
   def change
-    add_column :customer, :devices, :integer
+    add_column :customers, :devices, :integer
   end
 end
