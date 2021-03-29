d1d307093020281e42bc679ec2d376a5a573d801
Hannah Dwan <hannahdwan96@gmail.com>
2021-03-29 16:21:49 +0100

    fixup

--- acme-voip/config/routes.rb
+++ acme-voip/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :customers, [:index, :show, :create, :edit, :destroy]
-  resources :devices, [:index, :show, :create, :edit]
+  resources :customers, only: [:index, :show, :create, :edit, :destroy]
+  resources :devices, only: [:index, :show, :create, :edit]
 
 
 end
