007fbce20c0325441311923e527388d8ac6acfdf
Hannah Dwan <hannahdwan96@gmail.com>
2021-03-29 17:28:08 +0100

    Implements basic presence and postcode validation
    Like the commit name says, basic validation, regex would be far superior but => technical test

--- acme-voip/app/models/customer.rb
+++ acme-voip/app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ApplicationRecord
+  validates :name, :postcode, :contractEndDate, :contractTier, presence: true
+  validate :postcode_validation
+
+  def postcode_validation
+    unless postcode.length == 6 || postcode.length == 7
+      errors.add :postcode, :invalid, message: "This postcode is invalid"
+    end
+  end
 end
