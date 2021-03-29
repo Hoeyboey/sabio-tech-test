c97b72cf3b899467bab3740269099f06290d3d4f
Hannah Dwan <hannahdwan96@gmail.com>
2021-03-29 16:58:59 +0100

    Create path on devices controller
    Tests passing for create

--- acme-voip/app/controllers/devices_controller.rb
+++ acme-voip/app/controllers/devices_controller.rb
@@ -1,2 +1,20 @@
 class DevicesController < ApplicationController
+  def create
+    @device = Device.new(params[:device])
+    if @device.save
+      render :json => @device.as_json, :status => :created
+    end
+  end
+
+  def read
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
 end
