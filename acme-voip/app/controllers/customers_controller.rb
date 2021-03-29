38c413e553be2e9ad05c722ba4375db59142f560
Hannah Dwan <hannahdwan96@gmail.com>
2021-03-29 16:55:43 +0100

    Customers controller - create - tests passing
    Creates basic concept of create on customers controller

--- acme-voip/app/controllers/customers_controller.rb
+++ acme-voip/app/controllers/customers_controller.rb
@@ -1,2 +1,24 @@
 class CustomersController < ApplicationController
+  def create
+    @customer = Customer.new(params[:customer])
+    if @customer.save
+      render :json => @customer.as_json, :status => :created
+    end
+  end
+
+  def read
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
 end
